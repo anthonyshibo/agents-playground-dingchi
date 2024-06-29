00af5877fa1ba53ecf17ca35cd19d52761314a9d
anthonyshibo <133836502+anthonyshibo@users.noreply.github.com>
2024-06-30 02:35:43 +0800

    Update TranscriptionTile.tsx

--- src/transcriptions/TranscriptionTile.tsx
+++ src/transcriptions/TranscriptionTile.tsx
@@ -103,7 +103,7 @@ function segmentToChatMessage(
 ): ChatMessageType {
   const msg: ChatMessageType = {
     message: s.final ? s.text : `${s.text} ...`,
-    name: participant instanceof LocalParticipant ? "You" : "Agent",
+    name: participant instanceof LocalParticipant ? "Candidate:" : "Agent:",
     isSelf: participant instanceof LocalParticipant,
     timestamp: existingMessage?.timestamp ?? Date.now(),
   };
